be116990549451921f76961787eb42dd9e193234
Daniel Emery <danielremery@gmail.com>
2019-01-05 15:57:00 +1030

    Added linting and prettier configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
 app.listen(port, () => {
-  console.log(`Stingy Vegan listening on port ${port}`);
+  console.log(`Stingy Vegan listening on port ${port}`); // eslint-disable-line no-console
 });
