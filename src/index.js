e34744ab30cd1b34f5a2ef46b465304eca6927cc
Daniel Emery <danielremery@gmail.com>
2019-01-05 15:31:50 +1030

    Added placeholder express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Stingy Vegan listening on port ${port}`);
+});
