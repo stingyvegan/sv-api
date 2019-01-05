59f406736df7ae618a33f3966d157de3e6624983
Daniel Emery <danielremery@gmail.com>
2019-01-05 16:41:38 +1030

    Added initial cognito authenticated route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import express from 'express';
+import CognitoExpress from 'cognito-express';
 
-const app = express();
+import awsExports from './aws-exports';
+
 const port = 3000;
+const app = express();
+const authenticatedRoute = express.Router();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const cognitoExpress = new CognitoExpress({
+  region: awsExports.aws_region,
+  cognitoUserPoolId: awsExports.aws_user_pools_id,
+  tokenUse: 'access',
+  tokenExpiration: 3600000,
+});
+
+app.use('/api', authenticatedRoute);
+
+authenticatedRoute.use((req, res, next) => {
+  const accessTokenFromClient = req.headers.authorization.replace(
+    'Bearer ',
+    '',
+  );
+  cognitoExpress.validate(accessTokenFromClient, (err, response) => {
+    if (err) {
+      return res.status(401).send(err);
+    }
+
+    res.locals.user = response;
+    return next();
+  });
+});
+
+authenticatedRoute.get('/', (req, res) => {
+  res.send(res.locals.user);
 });
 
 app.listen(port, () => {
