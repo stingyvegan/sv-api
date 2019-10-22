a98b4e8a99a279a2b5e86ad38c584e835870acc0
Daniel Emery <daniel@sine.co>
2019-10-23 08:58:54 +1030

    Added additional sample products to seed

--- seeders/20190207122017-initial-products.js
+++ seeders/20190207122017-initial-products.js
@@ -3,18 +3,25 @@ const uuid = require('uuid/v4');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const now = new Date();
     const honestToGoodness = {
       name: 'Honest To Goodness',
       supplier_id: uuid(),
-      created_at: new Date(),
-      updated_at: new Date(),
+      created_at: now,
+      updated_at: now,
+    };
+    const veganPerfection = {
+      name: 'Vegan Perfection',
+      supplier_id: uuid(),
+      created_at: now,
+      updated_at: now,
     };
 
     const nutritionalYeast = {
       name: 'Nutritional Yeast',
       product_id: uuid(),
-      created_at: new Date(),
-      updated_at: new Date(),
+      created_at: now,
+      updated_at: now,
       is_discrete: false,
       unit_size: 100,
       unit_name: 'g',
@@ -22,9 +29,33 @@ module.exports = {
       total_cost: 1331,
       supplier_id: honestToGoodness.supplier_id,
     };
+    const beyondBurger = {
+      name: 'The Beyond Burger',
+      product_id: uuid(),
+      created_at: now,
+      updated_at: now,
+      is_discrete: true,
+      unit_size: 1,
+      unit_name: 'burger',
+      required_units: 42,
+      total_cost: 17100,
+      supplier_id: veganPerfection.supplier_id,
+    };
+    const vegoBar = {
+      name: '150g Vego Hazelnut Choc Bar',
+      product_id: uuid(),
+      created_at: now,
+      updated_at: now,
+      is_discrete: true,
+      unit_size: 1,
+      unit_name: 'bar',
+      required_units: 30,
+      total_cost: 13530,
+      supplier_id: veganPerfection.supplier_id,
+    };
 
-    await queryInterface.bulkInsert('supplier', [honestToGoodness], {});
-    return queryInterface.bulkInsert('product', [nutritionalYeast], {});
+    await queryInterface.bulkInsert('supplier', [honestToGoodness, veganPerfection], {});
+    return queryInterface.bulkInsert('product', [nutritionalYeast, beyondBurger, vegoBar], {});
   },
 
   down: (queryInterface, Sequelize) => {},
