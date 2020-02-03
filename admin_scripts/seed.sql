f4ca7db594cec1c31db14d3993b06a7d8912ab95
Daniel Emery <daniel@sine.co>
2020-02-04 08:51:33 +1030

    Added charset and collation to admin script

--- admin_scripts/seed.sql
+++ admin_scripts/seed.sql
@@ -1,4 +1,4 @@
 CREATE USER 'sv'@'%' IDENTIFIED BY 'sv';
 ALTER USER 'sv'@'%' IDENTIFIED WITH mysql_native_password BY 'sv';
-CREATE DATABASE sv;
+CREATE DATABASE sv CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 GRANT ALL PRIVILEGES ON sv.* TO 'sv'@'%';
