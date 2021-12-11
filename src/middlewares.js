210930ad526bc08d60a03f4cac1b24230efec79c
Daniel Emery <danielremery@gmail.com>
2021-12-11 23:42:15 +1030

    SV-27 Add middleware to send api version to client as header

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,18 +3,36 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import errors from './errors';
 
+const VERSION_HEADER = 'x-sv-api-version';
+
+function versionMiddleware(_request, response, next) {
+  // Set header
+  response.set(VERSION_HEADER, process.env.VERSION_TAG || 'local-dev');
+
+  // Expose header
+  const previousHeader = response.get('Access-Control-Expose-Headers');
+  response.set(
+    'Access-Control-Expose-Headers',
+    previousHeader
+      ? [previousHeader, VERSION_HEADER].join(',')
+      : VERSION_HEADER,
+  );
+  next();
+}
+
 export function addMiddlewares(express) {
   const corsOptions = {
     origin: process.env.CLIENT_URL,
   };
 
+  express.use(versionMiddleware);
   express.use(cors(corsOptions));
   express.use(bodyParser.json());
   expressWs(express);
 }
 
 export function addErrorHandlers(express) {
-  express.use((err, req, res) => {
+  express.use((err, _req, res) => {
     if (err instanceof errors.UnauthorisedError) {
       res.status(403).send('Missing required role');
     } else {
