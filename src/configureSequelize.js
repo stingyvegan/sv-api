5c9ded30f72079e9cdad0e73bf4c86c1929bbd0c
Daniel Emery <danielremery@gmail.com>
2022-03-10 17:22:59 +1030

    SV-38 Include additional details in connection log

--- src/configureSequelize.js
+++ src/configureSequelize.js
@@ -2,7 +2,9 @@ import models from '../models';
 import logger from './logger';
 
 export default async function configureSequelize() {
-  logger.info(`Attempting to connect to ${process.env.POSTGRES_HOST}`);
+  logger.info(
+    `Attempting to connect to postgres at ${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+  );
 
   const connection = await models.sequelize.authenticate();
   logger.info('Connection to postgres established successfully');
